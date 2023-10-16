f5c2f05b1fa1f9d069cddfce08cbd2a305a97acb
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-10-16 01:11:18 -0400

    All tests passing.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => {return res.json()}) //.json() returns an async func
+  .then((data) => renderBooks(data))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +15,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
